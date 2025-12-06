a40434d39a7cf7e78db4f2e98aa54fdff3169df4
Joe Bazydlo <32661309+Joester-Strudel@users.noreply.github.com>
2025-12-06 16:26:47 -0500

    apps added

---
+++ gc_donations/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class GcDonationsConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'gc_donations'
---
+++ gc_volunteers/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class GcVolunteersConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'gc_volunteers'
